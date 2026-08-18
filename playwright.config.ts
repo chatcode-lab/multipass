496b52932a7f7f60614fe7320000f10f75bf925b
Boris Karulin <borkasm@users.noreply.github.com>
2026-08-18 23:01:01 +0000

    Run mobile emulation with Chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,6 @@ export default defineConfig({
   },
   projects: [
     { name: "desktop", use: { ...devices["Desktop Chrome"] } },
-    { name: "mobile", use: { ...devices["iPhone 13"] } },
+    { name: "mobile", use: { ...devices["iPhone 13"], browserName: "chromium" } },
   ],
 });
